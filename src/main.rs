923d59d164044df132222b329f6a312ea25e01e6
SP0NJ4 <sponja.spognardi@gmail.com>
2023-01-17 21:25:07 -0300

    build(serenity): Added serenity boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
+// This is due to the package name: identifier names should be in snake case
 #![allow(non_snake_case)]
 
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenv::dotenv;
+use serenity::prelude::*;
+
+#[tokio::main]
+async fn main() {
+    dotenv().expect("Failed to load .env file");
+
+    let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN must be set");
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents)
+        .await
+        .expect("Error creating client");
 }
