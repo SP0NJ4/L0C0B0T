fe9e91357ae7a4f777746a6944eb52936b22fc3c
SP0NJ4 <sponja.spognardi@gmail.com>
2023-01-22 21:28:19 -0300

    feat(errors): Added error responses to commands

--- src/framework.rs
+++ src/framework.rs
@@ -1,4 +1,5 @@
 use serenity::framework::standard::macros::hook;
+use serenity::framework::standard::CommandError;
 use serenity::framework::StandardFramework;
 
 use crate::commands::music::MUSIC_GROUP;
@@ -13,6 +14,27 @@ async fn before(
     println!("Running {} command", _cmd_name);
     true
 }
+#[hook]
+async fn after(
+    _ctx: &serenity::client::Context,
+    _msg: &serenity::model::channel::Message,
+    cmd_name: &str,
+    cmd_result: Result<(), CommandError>,
+) {
+    println!("Finished running {} command", cmd_name);
+
+    if let Err(why) = cmd_result {
+        println!("Error running command: {:?}", why);
+
+        if let Err(why) = _msg
+            .channel_id
+            .say(&_ctx.http, format!("⚠️ **Error**: {}", why))
+            .await
+        {
+            println!("Error sending error message: {:?}", why);
+        }
+    }
+}
 
 // TODO: In the future, we'll replace StandardFramework with our own
 // custom framework.
@@ -20,6 +42,7 @@ pub fn create_framework() -> StandardFramework {
     StandardFramework::new()
         .configure(|c| c.prefix("!"))
         .before(before)
+        .after(after)
         .group(&TESTING_GROUP)
         .group(&MUSIC_GROUP)
 }
